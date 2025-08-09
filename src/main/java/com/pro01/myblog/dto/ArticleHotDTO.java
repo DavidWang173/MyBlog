12ca94cd0601277c9012152b128dcd2c7b800556
wyw <pianodrum@163.com>
2025-08-09 19:02:22 +0800

    热门文章榜单返回summary字段

--- src/main/java/com/pro01/myblog/dto/ArticleHotDTO.java
+++ src/main/java/com/pro01/myblog/dto/ArticleHotDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class ArticleHotDTO {
     private Long id;
     private String title;
+    private String summary;
     private String coverUrl;
     private String nickname;
     private String avatar;
