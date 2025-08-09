94db522caf3cf62d43f76f73e587b85a19ccf43e
wyw <pianodrum@163.com>
2025-08-09 17:54:04 +0800

    修改拦截器

--- src/main/java/com/pro01/myblog/interceptor/LoginInterceptor.java
+++ src/main/java/com/pro01/myblog/interceptor/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.pro01.myblog.interceptor;
 
+import com.pro01.myblog.exception.ForbiddenException;
+import com.pro01.myblog.exception.UnauthorizedException;
 import com.pro01.myblog.utils.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,26 +11,27 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.util.Map;
 
 public class LoginInterceptor implements HandlerInterceptor {
+
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 
         String token = request.getHeader("Authorization");
 
         if (!StringUtils.hasText(token)) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("未登录，缺少 Token");
-            return false;
+            throw new UnauthorizedException("未登录，缺少 Token");
         }
 
         Map<String, Object> claims = JwtUtil.parseToken(token);
 
         if (claims == null || claims.get("userId") == null) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write("Token 无效或已过期");
-            return false;
+            throw new UnauthorizedException("Token 无效或已过期");
         }
 
-        // ✅ 将解析出的 userId 和 role 放到 request 作用域
+        // 如果需要权限校验可以在这里判断
+         if (!"ADMIN".equals(claims.get("role"))) {
+             throw new ForbiddenException("权限不足");
+         }
+
         request.setAttribute("userId", ((Number) claims.get("userId")).longValue());
         request.setAttribute("role", claims.get("role"));
 
